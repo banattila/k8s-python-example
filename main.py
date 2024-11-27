6ad606014b6da20d738c3d89213c14a05b205325
banattila <banattila@protonmail.com>
2024-11-27 12:16:21 +0100

    Tried it

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+
+
+def any_example(any_string):
+	print(f"This is a string: {any_string}")
+
+
+if __name__ == "__main__":
+	while True:
+		time.sleep(10)
+		any_example("This is the main")
