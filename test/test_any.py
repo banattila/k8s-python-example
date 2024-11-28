61976dba1c7375150ef0dc3910e9099a6a16016f
banattila <banattila@protonmail.com>
2024-11-28 17:33:31 +0100

    test file renamed

---
+++ test/test_any.py
@@ -0,0 +1,10 @@
+import unittest
+from src.main import add
+
+
+class AnyTestClass(unittest.TestCase):
+
+    def test_add_function(self):
+        number_array = [0, 1, 2, 3]
+        result = add(number_array)
+        self.assertEqual(5, result)
