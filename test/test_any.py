fa0ba844c20d91b7f6a3e44b0258b202377234bf
banattila <banattila@protonmail.com>
2024-11-28 17:37:15 +0100

    test repaired

--- test/test_any.py
+++ test/test_any.py
@@ -7,4 +7,4 @@ class AnyTestClass(unittest.TestCase):
     def test_add_function(self):
         number_array = [0, 1, 2, 3]
         result = add(number_array)
-        self.assertEqual(5, result)
+        self.assertEqual(6, result)
